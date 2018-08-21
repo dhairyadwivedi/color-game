51eeb7b393873aeaa35e3cab2becaa601e429cc8
Dhairya <dhairiya96@gmail.com>
2018-08-21 12:36:32 +0530

    Update color-game.js

--- js/color-game.js
+++ js/color-game.js
@@ -1,1 +1,18 @@
 
+var numSquares = 6;
+var colors = [];
+var pickedColor;
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
+
+init();
+
+function init(){
+	setupModeButtons();
+	setupSquares();
+	reset();
+}
